80657036d841a40c8a077155dd24cb10eb2ad41c
Babu <babu.gangatharan@ing.com>
2024-05-16 12:56:04 +0200

    first commit

---
+++ BarcodeScanner/BarcodeScannerView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  BarcodeScanner
+//
+//  Created by Babu Gangatharan on 16/05/2024.
+//
+
+import SwiftUI
+
+struct BarcodeScannerView: View {
+    
+    @State private var scannedCode = ""
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                ScannerView(scannedCode: $scannedCode)
+                    .frame(maxWidth: .infinity, maxHeight: 300)
+                
+                Spacer()
+                    .frame(height: 60)
+                
+                Label("Scanned Barcode:", systemImage: "barcode.viewfinder")
+                    .font(.title)
+                
+                Text(scannedCode.isEmpty ? "Not yet scanned" : scannedCode)
+                    .bold()
+                    .font(.largeTitle)
+                    .foregroundStyle(scannedCode.isEmpty ? .red : .green)
+                    .padding()
+            }
+            .navigationTitle("Barcode Scanner")
+        }
+    }
+}
+
+#Preview {
+    BarcodeScannerView()
+}
